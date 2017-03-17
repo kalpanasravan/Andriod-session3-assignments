d1fdf63f31e7bb0ed4c0992676895055331dbafb
kalpanasravan <devi_kalpana@rediffmail.com>
2017-03-17 17:11:33 +0530

    Activity main.java file for dispplaying phne contatcs
    Activity main.java file for dispplaying phne contatcs

--- MainActivity.java
+++ MainActivity.java
@@ -1,34 +1,33 @@
-package com.example.mahathi.googlesearchimplicitintent;
+package com.example.mahathi.contactmenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.SearchManager;
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
-    private EditText editTextInput;
+
+    private static final int PICK_CONTACT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextInput = (EditText) findViewById(R.id.editText);
-    }
-
-
-    public void onSearchClick(View v)
-    {
-        try {
-            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-            String term = editTextInput.getText().toString();
-            intent.putExtra(SearchManager.QUERY, term);
-            startActivity(intent);
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
 
     }
+
+
+    public void showContactList(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent, PICK_CONTACT);
+    }
+
 }
